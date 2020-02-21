d3a4284edd18a913972f72db8f4f2f16e6a243b9
Matthew Slauson <slauson.matthew@gmail.com>
2020-02-20 19:37:23 -0600

    first commit

---
+++ src/test/java/io/blossom/microservices/expenses/ExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.blossom.microservices.expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
