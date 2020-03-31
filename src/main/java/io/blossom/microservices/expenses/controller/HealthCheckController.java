ddfe7d709f0d93730993f99b81915b82357038cd
Matthew Slauson <slauson.matthew@gmail.com>
2020-03-30 19:34:43 -0500

    add HealthCheckController

---
+++ src/main/java/io/blossom/microservices/expenses/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020. Blossom Budgeting LLC
+ * All Rights Reserved
+ */
+
+package io.blossom.microservices.expenses.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/")
+    public ResponseEntity<String> getHealthStatus() {
+        return ResponseEntity.ok("healthy");
+    }
+}
