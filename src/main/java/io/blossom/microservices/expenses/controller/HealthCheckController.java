d2746e0b8d846db3e1f749988b042be1b717ebc7
Matthew Slauson <slauson.matthew@gmail.com>
2020-06-14 10:17:21 -0500

    updates

--- src/main/java/io/blossom/microservices/expenses/controller/HealthCheckController.java
+++ src/main/java/io/blossom/microservices/expenses/controller/HealthCheckController.java
@@ -15,4 +15,9 @@ public class HealthCheckController {
     public ResponseEntity<String> getHealthStatus() {
         return ResponseEntity.ok("healthy");
     }
+
+    @GetMapping("/expenses")
+    public ResponseEntity<String> getHealthStatus2() {
+        return ResponseEntity.ok("healthy");
+    }
 }
